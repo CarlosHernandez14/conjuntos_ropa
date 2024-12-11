7e32777aa44a59674d782f8b744c646edfa5aca1
CarlosHernandez14 <carloskvpchc@gmail.com>
2024-12-10 18:58:07 -0600

    First commit

---
+++ src/main/java/com/mycompany/conjuntos_ropa/Conjuntos_ropa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conjuntos_ropa;
+
+/**
+ *
+ * @author carlo
+ */
+public class Conjuntos_ropa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
